2c1c9a3b40ab23ec91438318efa4ace9682080d6
Guan <5354.john@gmail.com>
2020-07-28 12:32:22 +1200

    Q1

---
+++ Q1.py
@@ -0,0 +1,36 @@
+import sys
+import string
+def read(txt):
+	file_list = []
+	for line in txt:
+		file_list.append(line)
+
+	current_line = 0
+	read_line_num = int(file_list[current_line])
+	current_line += 1
+	out_put_l = []
+
+	while(True):
+		if read_line_num == 0:
+			out_put_l.append(read_line_num)
+			return out_put_l
+
+		out_put_l = []
+
+		out_put_l.append(read_line_num)
+		for i in range(read_line_num*2):
+			out_put_l.append(file_list[current_line].split())
+			current_line += 1
+		read_line_num = int(file_list[current_line])
+		current_line += 1
+
+	
+
+
+def main():
+	txt_input = sys.stdin
+	for i in read(txt_input):
+		if isinstance(i, int): print(i)
+		else: print(string.join(i))
+
+main()
